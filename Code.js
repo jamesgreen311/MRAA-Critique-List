e9e6369916fe80bb24d304bcc17af551471de8cf
capn <jamesgreen.311@gmail.com>
2021-02-18 11:46:10 -0500

    Test read, write to google sheets

--- Code.js
+++ Code.js
@@ -1,3 +1,17 @@
 function doGet() {
   
+    const response = [{status: "cool"}];
+
+    return ContentService
+        .createTextOutput(JSON.stringify(response))
+        .setMimeType(ContentService.MimeType.JSON);
+
+}
+
+function doPost(e) {
+    const body = e.postData.contents;
+    const bodyJSON = JSON.parse(body);
+    const ss = SpreadsheetApp.openById('11mfnGVNA2PhCs5hsp5-bmKhndv7nFg-ZiVVKf1caTqg');
+    const ws = ss.getSheetByName('Sheet2');
+    ws.appendRow([bodyJSON.name]);
 }
